070082a8b5e38684662000c608fa3895b3f78286
CasperAntonPoulsen <casperantonpoulsen@gmail.com>
2021-10-13 15:33:05 +0200

    Implemented server

--- server/server.go
+++ server/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 
-	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc"
 
 	pb "github.com/CasperAntonPoulsen/MiniProject2/proto"
 )
@@ -13,35 +15,51 @@ const port = ":8080"
 type chittyChatServer struct {
 	pb.UnimplementedChittyChatServer
 	usersInChat []*pb.User
-	observers []
+	observers   []pb.ChittyChat_ReceiveMsgServer
 }
-
 
 var clocks []int32
 
-
 func (s *chittyChatServer) Join(ctx context.Context, user *pb.User) (*pb.JoinResponse, error) {
 	for _, _user := range s.usersInChat {
-		if proto.Equal(user.Name, _user.Name) {
+		if user.Name == _user.Name {
 			return &pb.JoinResponse{Error: 1,
-									Msg: "User already exists"},
-				   nil
+					Msg: "User already exists"},
+				nil
 		}
 	}
 
 	s.usersInChat = append(s.usersInChat, user)
 	return &pb.JoinResponse{Error: 0,
-							Msg: "Success"},
-		   nil
+			Msg: "Success"},
+		nil
 }
 
 func (s *chittyChatServer) getAllUsers(ctx context.Context, empty *pb.Empty) (*pb.UserList, error) {
-	return &pb.UserList{Users:s.usersInChat}, nil
+	return &pb.UserList{Users: s.usersInChat}, nil
 }
 
 func (s *chittyChatServer) receiveMsg(empty *pb.Empty, stream pb.ChittyChat_ReceiveMsgServer) error {
-
+	s.observers = append(s.observers, stream)
 	return nil
 }
 
-func (s *chittyChatServer) sendMsg(ctx context.Context, )
+func (s *chittyChatServer) sendMsg(ctx context.Context, chatMessage *pb.ChatMessage) (*pb.Empty, error) {
+	for _, observer := range s.observers {
+		observer.Send(chatMessage)
+	}
+	return &pb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChittyChatServer(s, &chittyChatServer{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
